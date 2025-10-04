99ae79ff7338ceb736fabcdcf91435e5694a0ece
zaia-1 <azizmohammed24@hotmail.com>
2025-10-04 23:23:32 +0800

    Add files via upload

---
+++ EvilPython.py
@@ -0,0 +1,4 @@
+# 10/04/2025 I will either learn python and be ready or i'll be cooked
+
+print("\033[31mEvil Python: Goodbye World")
+print("your terminal is forever RED NOW MUHAHAHAHAHAHA (don't restart me please)")
